6c38fe748e0d1ef5655964c442ff90196f3b02ed
Sumit <sumitvij11+github@gmail.com>
2024-12-13 09:15:14 +0000

    Cleanup

--- tests/lora/test_ultravox.py
+++ tests/lora/test_ultravox.py
@@ -6,7 +6,6 @@ import pytest
 import vllm
 
 from transformers import  AutoTokenizer
-from vllm.assets.audio import AudioAsset
 from vllm.lora.request import LoRARequest
 from vllm.platforms import current_platform
 
@@ -15,21 +14,8 @@ MODEL_NAME = "fixie-ai/ultravox-v0_3"
 VLLM_PLACEHOLDER = "<|reserved_special_token_0|>"
 
 EXPECTED_OUTPUT = [
-    "Mary had a little lamb"
+    "Fool mate"
 ]
-
-def load_test_audio() -> List[AudioAsset]:
-    return [AudioAsset("questions.mp3")]
-
-@pytest.fixture(scope="session")
-def audio_assets()->List[AudioAsset]:
-    from vllm.assets.audio import AudioAsset
-    return [AudioAsset("mary_had_lamb"), AudioAsset("winning_call")]
-
-@pytest.fixture(scope="module", params=("mary_had_lamb", "winning_call"))
-def audio(request):
-    from vllm.assets.audio import AudioAsset
-    return AudioAsset(request.param)
 
 def _get_prompt(audio_count, question, placeholder):
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
@@ -42,18 +28,15 @@ def _get_prompt(audio_count, question, placeholder):
                                          tokenize=False,
                                          add_generation_prompt=True)
 
-def do_sample(llm: vllm.LLM, lora_path: str, lora_id: int, audio_assets: List[AudioAsset]) -> List[str]:
+def do_sample(llm: vllm.LLM, lora_path: str, lora_id: int) -> List[str]:
     sampling_params = vllm.SamplingParams(
         temperature=0,
-        max_tokens=100,
+        max_tokens=1000,
     )
 
     inputs = [{
-        "prompt":_get_prompt(1, "Describe the audio above.", VLLM_PLACEHOLDER),
-        "multi_modal_data": {
-            "audio": a.audio_and_sample_rate
-        },
-    } for a in audio_assets]
+        "prompt":_get_prompt(1, "Tell me about a silly chess move in 20 words", VLLM_PLACEHOLDER),
+    }]
 
     outputs = llm.generate(
         inputs,
@@ -70,19 +53,19 @@ def do_sample(llm: vllm.LLM, lora_path: str, lora_id: int, audio_assets: List[Au
     return generated_texts
 
 
-def test_fixie_lora(minicpmv_lora_files):
+def test_fixie_lora(llama3_1_8b_chess_lora):
     llm = vllm.LLM(
         MODEL_NAME,
         max_num_seqs=2,
         enable_lora=True,
         max_loras=4,
-        max_lora_rank=64,
+        max_lora_rank=128,
         trust_remote_code=True,
         dtype="bfloat16",
         max_model_len=4096,
         enforce_eager=True
     )
-    output1 = do_sample(llm, minicpmv_lora_files, lora_id=1, audio_assets=load_test_audio())
+    output1 = do_sample(llm, llama3_1_8b_chess_lora, lora_id=1)
     for i in range(len(EXPECTED_OUTPUT)):
         assert EXPECTED_OUTPUT[i].startswith(output1[i])
     return None
